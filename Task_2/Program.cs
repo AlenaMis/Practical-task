16ea1ad98c1d116b94e1f329145c2c58c6027b8e
Alena <alenamis90@mail.ru>
2023-02-11 23:33:17 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число d: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
